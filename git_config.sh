2f5ade889bae6e079f2551a7088c068fd782a34a
Tyxik <tylmaxim@gmail.com>
2024-11-04 18:49:58 +0100

    Fotkos

---
+++ git_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to log into GitHub
+
+# Prompt for GitHub username and email
+echo "Logging into GitHub..."
+
+# Set GitHub username
+git config --global user.name "Tyxik"
+
+# Set GitHub email
+git config --global user.email "tylmaxim@gmail.com"
+
+echo "GitHub configuration set for user Tyxik with email tylmaxim@gmail.com."
